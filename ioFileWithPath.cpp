231e51eb7976530748ec6dbb3bda20ed69907e46
rukarajinbelajar <maar1101dnd@gmail.com>
2026-06-01 15:00:13 +0700

    menutup file apabila telah selesai di tulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -30,4 +30,6 @@ int main()
         //menulis dan memasukkan nilai dari baris ke dalam file
         outfile << baris << endl;
     }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
 }
