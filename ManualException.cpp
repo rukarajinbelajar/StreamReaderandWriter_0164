4678734106fdd52b5f485a2a6646be9a2495068b
rukarajinbelajar <maar1101dnd@gmail.com>
2026-06-01 15:10:25 +0700

    membuat isi dari try untuk pengujian error

--- ManualException.cpp
+++ ManualException.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main(
+int main()
+{
+    //membuat isi dari try untuk pengujian error
+    try
     {
-        
+        cout << "Selamat belajar di prodi TI  UMY" << endl;
     }
-)
+}
