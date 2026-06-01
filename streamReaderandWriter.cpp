96bbaae7d42c6de81c33ca21cc2c9c8fc88b4776
rukarajinbelajar <maar1101dnd@gmail.com>
2026-06-01 15:19:33 +0700

    kondisi false jika baris hanya berisi  karakter q

--- streamReaderandWriter.cpp
+++ streamReaderandWriter.cpp
@@ -22,5 +22,7 @@ int main()
         cout << "- ";
         //mendapat setiap karakter dalam satu baris
         getline(cin, baris);
+        //kondisi false jika baris hanya berisi  karakter q
+        if (baris == "q") break;
     }
 }
